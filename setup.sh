207dcd71e5c6fcb4ec50ac9dd99805d39d38312b
yewton <yewton@gmail.com>
2014-04-19 19:31:10 +0900

    tmux-poweline ではなく Powerline を使うように変更

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-FILES=(".tmux-powerlinerc" ".tmux.conf" ".zshrc" "tmux-powerline/themes/clarity.sh")
+FILES=(".pydistutils.cfg" ".tmux.conf" ".zshrc" ".config/powerline")
 
-(cd `dirname $0`
- PWD=`pwd`
- for FILE in ${FILES[@]}; do
-     TO=$PWD/$FILE
-     FROM=~/$FILE
-     if [ -f $FROM ]; then
-         echo "${FROM} already exists."
-     else
-         echo "${FROM} -> ${TO}"
-         ln -s $TO $FROM
-     fi
- done)
+(
+    cd `dirname $0`
+    PWD=`pwd`
+    for FILE in ${FILES[@]}; do
+        TO=$PWD/$FILE
+        FROM=~/$FILE
+        if [ -e $FROM ]; then
+            echo "${FROM} already exists."
+        else
+            echo "${FROM} -> ${TO}"
+            ln -s $TO $FROM
+        fi
+    done
+)
